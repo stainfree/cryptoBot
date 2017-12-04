36c4f6daf1c42706db7de119dad2cfc0b294c3b1
Mike Lin <mike.lin@caseware.cwi.local>
2017-12-04 17:47:36 -0500

    completed GdaxTradeBlock.parseTrades, incomplete populateTrades

--- cryptoBot.js
+++ cryptoBot.js
@@ -14,6 +14,7 @@ var TIME_FRAME = 60000*5; //60000*x = x minutes
 var VOLUME_THRESHOLD = 500;
 var DUMPALERT_TOKEN = 'aiaymynvmwaxovky5z7uagciqv2opj'; //Pushover app token
 var MIKE_TOKEN = 'ujt68kne2nc2ye6wav5pund5v61fuz'; //Pushover user Key
+var DEFAULT_INFO = {base: 'BTC', counter: 'USD'};
 
 /****************************************
 ***************** CLASSES ***************
@@ -37,10 +38,30 @@ var Orderbook = function(pair) {
   // };
 };
 
+//Generic class for holding info about a single trade
+var Trade = function(tradeInfo) {
+  this.side = tradeInfo.side;
+  this.base = tradeInfo.base || defaultInfo.base;
+  this.counter = tradeInfo.counter || defaultInfo.counter;
+  this.price = tradeInfo.price;
+  this.size = tradeInfo.size;
+};
+
+Trade.prototype.getValue = function() {
+  return this.size * this.price;
+};
+
+Trade.prototype.getPair = function() {
+  return this.base + '/' + this.counter;
+};
+
 //Class for holding aggregate trades in a given time frame
-var Tradeblock = function(tradesFrom, tradesTo) {
+//base and counter are optional
+var Tradeblock = function(tradesFrom, tradesTo, base, counter) {
   this.tradesFrom = tradesFrom;
   this.tradesTo = tradesTo;
+  this.base = base || DEFAULT_INFO.base;
+  this.counter = counter || DEFAULT_INFO.counter;
   var trades = [];
 
   this.addTrade = function(trade) {
@@ -49,43 +70,50 @@ var Tradeblock = function(tradesFrom, tradesTo) {
   };
 };
 
-//Generic class for holding info about a single trade
-var Trade = function(tradeInfo) {
-  this.side = tradeInfo.side;
-  this.base = tradeInfo.base || defaultInfo.base;
-  this.counter = tradeInfo.counter || defaultInfo.counter;
-  this.ratio = tradeInfo.ratio;
-  this.amount = tradeInfo.amount;
-};
-
-Trade.prototype.getValue = function() {
-  return this.amount * this.ratio;
-};
-
-Trade.prototype.getPair = function() {
-  return this.base + '/' + this.counter;
-};
-
 var GdaxTradeBlock = function(tradesFrom, tradesTo) {
   this.prototype = new Tradeblock(tradesFrom, tradesTo);
 };
 
-GdaxTradeBlock.prototype.parsePage = function(trades) {
-
+//data is from getProductTrades
+//Parses in data.body
+GdaxTradeBlock.prototype.parseTrades = function(data) {
+  var result = {
+    earliest: null,
+    latest: null
+  };
+  var tradeArr = JSON.parse(data.body);
+  tradeArr.forEach(function(trade) {
+    var tradeTimeObj = new Date(trade.time);
+    if (!result.earliest || tradeTimeObj < result.earliest) {
+      result.earliest = tradeTimeObj;
+    }
+    if (!result.latest || tradeTimeObj > result.latest) {
+      result.latest = tradeTimeObj;
+    }
+    if(tradeTimeObj <= this.tradesTo && tradeTimeObj >= this.tradesFrom) {
+      trade.base = this.base;
+      trade.counter = this.counter;
+      this.addTrade(new Trade(trade));
+    }
+  });
 };
 
-GdaxTradeBlock.prototype.populateTrades = function(client) {
-  var q = $q.defer();
+//recursivePromise is not meant to be used as an arg
+//TODO(continue here)
+GdaxTradeBlock.prototype.populateTrades = function(client, recursiveQ) {
+  var q = recursiveQ || $q.defer();
   client.getProductTrades(function(error, result) {
+    var parseRes = this.parseTrades(result);
+    if (parseRes.latest < )
+    this.populateTrades
     q.resolve(result);
   });
+  return q.promise;
 };
 
 /****************************************
 *************** FUNCTIONS ***************
 ***************************************/
-
-parse
 
 var getGdaxTradeBlock = function(client, tradesFrom, tradesTo) {
   var q = $q.defer();
@@ -99,14 +127,10 @@ var getGdaxTradeBlock = function(client, tradesFrom, tradesTo) {
 ****************** MAIN ****************
 ***************************************/
 
-var defaultInfo = {};
-var setDefault = function(prop, val) {
-};
-
 //Init and test
 var gdaxClient = new gdax.PublicClient();
 var currentTime = new Date().getTime() / 1000;
-var trades;
+var tradeBlock = new GdaxTradeBlock(currentTime - 600, currentTime);
 getGdaxTradeBlock(gdaxClient, currentTime - 600, currentTime).then(function(data) {
-  trades = data;
+  tradeBlock.parseInPage(data);
 });
